24385306aeaff6be41a79ea2f1d85948ac7ece69
Manik MT <manikmt01@gmail.com>
2024-07-10 18:19:00 +0600

    udpate

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx,css}',
+    './node_modules/flowbite/**/*.js',
+  ],
+  theme: {
+    extend: {
+      spacing: {
+        128: '32rem',
+        144: '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+    },
+  },
+  plugins: [require('flowbite/plugin')],
+};
